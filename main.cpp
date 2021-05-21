b9d9d50a41ea7ad5c76d8c4aaba12cefe5f570f5
Jose Terrones <66368520+RadeonXYZ@users.noreply.github.com>
2021-05-21 16:55:40 -0500

    main.cpp modificado
    by jose terrones

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "bst.h"
 #include "type.h"
-
+//by jose terrones
 using namespace std;
 
 int main()
@@ -16,8 +16,39 @@ int main()
   bst.insert(40);
   bst.insert(100);
 
-  cout << "Ascendente ..." << endl;
-  bst.VisitNodesAsc();
-  cout << "Descendente ..." << endl;
-  bst.VisitNodesDesc();
+
+  cout << "Iterator Descendente..." << endl;
+
+  bst.Iterator_descendente();
+
+  cout << endl<< "Iterator Ascendente..." << endl;
+
+  bst.Iterator_ascend();
+
+  cout<<endl<< "Inorder ..." << endl;
+  bst.Inorder();
+  cout<<endl << "Preorder ..." << endl;
+  bst.Preorder();
+  cout<<endl << "Postorder ..." << endl;
+  bst.Postorder();
+
+  cout <<endl<< "Preorder 2d ..." << endl;
+  bst.Preorder2d();
+  cout<< endl << "Inorder en 2d ..." << endl;
+  bst.Inorder2d();
+  cout << endl<< "Postorder en 2d ..." << endl;
+  bst.Postorder2d();
+
+  bst.Borrar(100);
+  cout <<  "" << endl;
+
+  cout<< endl<< "Se borro el dato 100..." << endl;
+  cout<< "Para comprobar q se borro el dato..." << endl;
+  //se usa Inorder para ver si se elimino el dato
+  cout<<endl << "Inorden ..." << endl;
+  bst.Inorder();
+  /*
+  cout << "Imprimir BFS..." << endl;
+  bst.BFS();
+  */
 }
